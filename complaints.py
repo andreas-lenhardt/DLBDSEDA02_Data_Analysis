8bcf9aba9219d5685fc1daeeb60b679556efdac9
Andreas Lenhardt <andreas.lenhardt@iubh.de>
2023-04-09 20:14:07 +0200

    Further editing

--- complaints.py
+++ complaints.py
@@ -1,4 +1,9 @@
 import pandas as pd
+import re
+import string
+
+
+#TODO: Doppelte IDs entfernen
 
 #%% 1 Load and prepare the source data
 df = pd.read_csv('CDPH_Environmental_Complaints.csv')
@@ -10,7 +15,7 @@ df = df.dropna(subset=['COMPLAINT DETAIL'])
 df['COMPLAINT DATE']= pd.to_datetime(df['COMPLAINT DATE'])
 
 # Remove rows depending on the year of the complaint
-df = df[df['COMPLAINT DATE'].dt.year >= 2022]
+df = df[df['COMPLAINT DATE'].dt.year >= 2023]
 
 # Copy selected columns to a new dataframe
 df2 = df[['COMPLAINT ID'
@@ -23,9 +28,25 @@ cols = ['id', 'type', 'complaint', 'date']
 df2.columns = cols
 
 # Recreate the index
-df2.reset_index(drop=True, inplace=True)
+#df2.reset_index(drop=True, inplace=True)
 
 
-#%% 2.1 Case Normalization
-df2.loc[:, 'complaint'] = df2['complaint'].map(str.lower)
+#%% 2.1 Case normalization
+def lower_complaint(complaint):
+    complaint = complaint.lower()
+    return complaint
+
+def remove_punctuation(complaint):
+    complaint = complaint.translate(str.maketrans('', '', string.punctuation))
+    return complaint
+
+def remove_numbers(complaint):
+    complaint = re.sub('[0-9]', '', complaint)
+    return complaint
+
+df2.loc[:, 'complaint'] = df2['complaint'].apply(lambda x: lower_complaint(x))
+
+df2.loc[:, 'complaint'] = df2['complaint'].apply(lambda x: remove_punctuation(x))
+
+df2.loc[:, 'complaint'] = df2['complaint'].apply(lambda x: remove_numbers(x))
 
