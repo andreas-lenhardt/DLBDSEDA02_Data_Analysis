e4234a99925625b756b249fe700104a814c19e5a
Andreas Lenhardt <andreas.lenhardt@iubh.de>
2023-04-15 20:20:40 +0200

    LSA added
    plus YAML configuration for Python environment

--- complaints.py
+++ complaints.py
@@ -5,9 +5,10 @@ from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.decomposition import TruncatedSVD
 
 
-#%% 1 Load and prepare the source data
+#%% 1 Data Loading and Preparing
 df = pd.read_csv("CDPH_Environmental_Complaints.csv")
 
 # Remove rows with no 'complaint detail'
@@ -22,7 +23,7 @@ df.drop_duplicates(
 df["COMPLAINT DATE"] = pd.to_datetime(df["COMPLAINT DATE"])
 
 # Remove rows depending on the year of the complaint
-df = df[df["COMPLAINT DATE"].dt.year >= 2022]
+df = df[df["COMPLAINT DATE"].dt.year >= 2013]
 
 # Copy selected columns to a new dataframe
 df2 = df[
@@ -37,7 +38,7 @@ df2.columns = cols
 df2.reset_index(drop=True, inplace=True)
 
 
-#%% 2.1 Text cleaning
+#%% 2.1 Text Cleaning
 def lower_complaint(complaint):
     complaint = complaint.lower()
     return complaint
@@ -76,12 +77,60 @@ df2.loc[:, "cP_list"] = df2["cP_list"].apply(lambda x: lemmatize_complaints(x))
 df2.loc[:, 'cP_list'] = df2['cP_list'].apply(lambda x: ' '.join(x))
 
 
-vect_bow = CountVectorizer(ngram_range=(1,1), max_features=20,min_df=1, max_df=1.0)
-matrix_bow = vect_bow.fit_transform(df2["cP_list"])
-data_bow = pd.DataFrame(matrix_bow.toarray(),columns=vect_bow.get_feature_names_out())
+#%% 2.2 Feature Extraction (BoW, TF-IDF)
+# vect_bow = CountVectorizer(ngram_range=(1,1),
+#                             max_features=100,
+#                            # min_df=1,
+#                            # max_df=1.0
+#                            )
+# matrix_bow = vect_bow.fit_transform(df2["cP_list"])
+# data_bow = pd.DataFrame(matrix_bow.toarray(),
+#                         columns=vect_bow.get_feature_names_out())
 
 
-vect_tfidf = TfidfVectorizer(min_df=1)
+vect_tfidf = TfidfVectorizer(ngram_range=(1, 1),
+                             max_features=1000,
+                             # min_df=0.01,
+                             # max_df=0.95
+                             )
 matrix_tfidf = vect_tfidf.fit_transform(df2["cP_list"])
-data_tfidf = pd.DataFrame(matrix_tfidf.toarray(), columns=vect_tfidf.get_feature_names_out())
+data_tfidf = pd.DataFrame(matrix_tfidf.toarray(),
+                          columns=vect_tfidf.get_feature_names_out())
+
+data_tfidf.to_csv("test.csv", sep=";")
+
+
+#%% 2.3 Topic Modeling (LSA, LDA)
+lsa_model = TruncatedSVD(n_components=10, algorithm='randomized', n_iter=10, random_state=42)
+
+lsa_top=lsa_model.fit_transform(matrix_tfidf)
+
+print(lsa_top)
+print(lsa_top.shape)  # (no_of_doc*no_of_topics)
+
+l=lsa_top[0]
+print("Document 0 :")
+for i,topic in enumerate(l):
+    print("Topic ", i ," : ", topic*100)
+
+
+
+
+print(lsa_model.components_.shape) # (no_of_topics*no_of_words)
+print(lsa_model.components_)
+
+
+
+
+# most important words for each topic
+vocab = vect_tfidf.get_feature_names_out()
+
+for i, comp in enumerate(lsa_model.components_):
+    vocab_comp = zip(vocab, comp)
+    sorted_words = sorted(vocab_comp, key= lambda x:x[1], reverse=True)[:10]
+    print("Topic " + str(i) + ": ")
+    for t in sorted_words:
+        print(t[0], end=" ")
+    print("\n")
+
 
